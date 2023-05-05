677213e75adeedb1eb7199af75bd77038fed98a0
Smit1995 <129458113+Smit1995@users.noreply.github.com>
2023-05-05 12:45:33 -0500

    Se realzo la interfaz de inicio de seion, administrativo Asignacion y servicio al usuario, junto con la interfaz de regisstro

--- src/components/cinini.jsx
+++ src/components/cinini.jsx
@@ -5,7 +5,7 @@ const Contraseña = () => {
     const [showPwd, setShowPwd] = useState(true)
     return  <div> <div className= "username1">
     <div className="position-absolute pointer pwd-icon" onClick={() => setShowPwd(!showPwd)} style={{float:"23%"}}>
-    <input type={showPwd ? "text" : "Contraseña"} className="reglon1" placeholder="Contraseña" id="Contraseña" required />
+    <input type={showPwd ? "text" : "password"} className="reglon1" placeholder="Contraseña" id="password" required />
       {showPwd ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.2rem"}>
         <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
         <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
