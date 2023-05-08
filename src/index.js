fb08a000e36dfc2c21210ea838943beb089ee1fd
Milena Ortiz <85770545+mmilenaog@users.noreply.github.com>
2023-05-07 20:39:06 -0500

    Se incluyen componentes, rutas y estilos Se eliminan rutas no utilizadas Se incluyen extensiones requeridas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* Se inicia el proyecto creando el index.js como ruta principal en donde se determina el ruteo para todas las páginas
+   Se importan las extensiones requeridas y los estilos  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css/estilo.css";
+import "./styles.css/inicio.css";
+import "./styles.css/container.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/Home";
+import Registro from "./routes/Registro";
+import InicioSesion from "./routes/InicioSesion";
+import Error from "./routes/Error";
+import IngresoAdministrativo from "./routes/IngresoAdministrativo";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/Registro.jsx",
+    element: <Registro />,
+  },
+  {
+    path: "/InicioSesion.jsx",
+    element: <InicioSesion />,
+  },
+  {
+    path: "/IngresoAdministrativo.jsx",
+    element: <IngresoAdministrativo />,
+  },
+  ]);
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
